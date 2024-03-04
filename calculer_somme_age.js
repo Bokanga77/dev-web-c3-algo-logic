a24cb36ca2bd87ef0f715b20580b3a328a1eaa15
Bokanga77 <moisebokanga77@gmail.com>
2024-03-04 14:45:03 +0100

    resulution calcul de la somme d'âge

--- calculer_somme_age.js
+++ calculer_somme_age.js
@@ -8,8 +8,20 @@ const personnes = [
     { nom: 'Charlie', age: 20 }
   ];
   const ageMinimum = 25;
-
-
   /**
    * Ex: Somme des âges : 75
    */
+  // Trier les utilisateurs par ordre alphabétique
+personnes.sort((a, b) => a.nom.localeCompare(b.nom));
+
+// Écrire une fonction qui calcule la somme de tous les âges dans le tableau
+function calculerSommeAges(personnes) {
+  let somme = 0;
+  personnes.forEach(personne => {
+    somme += personne.age;
+  );
+  return somme;
+
+
+console.log(calculerSommeAges(personnes));
+
